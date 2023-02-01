9fa508a20fc6a9d9d7904a72512d08d80f87f9a0
Anshuman202 <asanshuman2@gmail.com>
2023-02-01 19:54:43 +0530

    updated for SQL-Employee-Payroll-Service

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Employee_Payroll
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll ADO.NET problem.");
+        }
+    }
+}
